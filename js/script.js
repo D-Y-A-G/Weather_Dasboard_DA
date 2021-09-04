afe29feff2ae5a2cd3ae35487e7645cc5fea9f7e
Denis Arce <dnsarce21@gmail.com>
2021-09-04 08:42:41 -0500

    working on js to get weather API data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 //setting up openweather APIKey and url
 
-var APIKey ="329a430c78a8cebf9c6c895dedfc7209";
-var city;
-var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var APIKey ="f11d17f447a311709f9c2eb37981ebff";
+var city = "";
+var searchButton = document.querySelector(".searchbtn")
+
+function getApi() {
+   //using url link to get weather data
+    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
 
 //need to fetch data from openweather API
+    fetch(queryUrl)
+    .then(function (response){
+        return (response);
+    })
+    .then(function (data){
+        console.log(data)
+    
+    });
+    
+    
+}
+
+searchButton.addEventListener("click", getApi)
 
 //User inputs the name of the city that they would like to check current weather
 
