a23316dcccb88ad2fcfeb34d969cf5ad5928d906
Denis Arce <dnsarce21@gmail.com>
2021-09-06 12:20:34 -0500

    working on getting weather data

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
 //setting up openweather APIKey and url
 
-var APIKey ="f11d17f447a311709f9c2eb37981ebff";
-var city = "";
-var searchButton = document.querySelector(".searchbtn")
+var APIKey = "329a430c78a8cebf9c6c895dedfc7209";
+var cityEl = document.querySelector("#cityName"); //need to change this to get val from user input on search bar
+var searchButton = document.querySelector("#button-addon2");
+var tempEl = document.querySelector("#currentTemp");
+var city = [];
+//need to get user input to add to url data
 
-function getApi() {
-   //using url link to get weather data
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+var getUserInput = function (event) {
+  event.preventDefault();
+  var cityInput = cityEl.value.trim();
+  if (cityInput) {
+    getWeather(cityInput);
+    city.unshift({ cityInput });
+    cityEl.value = "";
+  }
+};
 
-//need to fetch data from openweather API
-    fetch(queryUrl)
-    .then(function (response){
-        return (response);
+function getWeather() {
+  //using url link to get weather data
+  var queryUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+
+  //need to fetch data from openweather API
+  fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function (data){
-        console.log(data)
-    
+    .then(function (data) {
+      console.log(data);
+
+      //for (var i = 0; i < data.length; i++){
+      //var showCity = document.createElement("div");
+      //showCity.textContent = data[i].main;
+
+      // }
     });
-    
-    
 }
 
-searchButton.addEventListener("click", getApi)
+searchButton.addEventListener("click", getWeather);
 
 //User inputs the name of the city that they would like to check current weather
 
